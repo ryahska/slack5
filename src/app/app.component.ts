bfd8ffefe5dd854ee31e600e309ffa99486f55c7
Akshay R <cbptrm@gmail.com>
2020-11-02 10:37:53 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MediaChange ,MediaObserver} from '@angular/flex-layout';
+import { Component ,OnInit,OnDestroy} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'slack5';
-}
+
+export class AppComponent implements OnInit,OnDestroy{
+  title='slack';
+  mediaSub:Subscription;
+  deviceXs:boolean;
+  constructor(public mediaObserver:MediaObserver)
+  {}
+  ngOnInit(){
+    this.mediaSub=this.mediaObserver.media$.subscribe((result:MediaChange)=>{
+     
+      this.deviceXs= (result.mqAlias==='sm')? true:false;
+    })
+  }
+  ngOnDestroy(){
+    this.mediaSub.unsubscribe();
+  }
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { NgModule} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
